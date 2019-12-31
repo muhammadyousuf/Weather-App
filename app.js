2edb2772d1429bd456685356b94ed8faca5c0168
muhammad yousuf <muhammadyousuf327@gmail.com>
2019-12-31 10:33:43 +0500

    get current temp

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const request = require("request");
 
 const url =
-  "https://api.darksky.net/forecast/425ac3054ab42e0b7eaa3a53faf865a4/37.8267,-122.4233";
+  "https://api.darksky.net/forecast/425ac3054ab42e0b7eaa3a53faf865a4/24.8607,67.0011";
 
-request({ url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+request({ url, json: true }, (error, response) => {
+  const { currently, daily } = response.body;
+  console.log(
+    daily.data[0].summary +
+      " It is currently " +
+      currently.temperature +
+      " degress out. There is a " +
+      currently.precipProbability +
+      "% chance of rain."
+  );
 });
